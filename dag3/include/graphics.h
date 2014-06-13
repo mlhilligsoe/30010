66f1ccb286a66d2c64c119bcd371d29af18a8961
s130294 <lennartk@lk-macbook.local>
2014-06-13 11:35:24 +0200

    graphics
    Started graphics.c file and updated graphics.h

--- dag3/include/graphics.h
+++ dag3/include/graphics.h
@@ -1,4 +1,7 @@
 #include <eZ8.h>             // special encore constants, macros and flash routines
 #include <sio.h>   
+#include "struct.h"
 
-
+void redrawPlayer(char x.old, char y.old, char x.new, char y.new);
+void redrawBall(char x.old, char y.old, char x.new, char y.new);
+void redrawBlock(struct Block* block);
