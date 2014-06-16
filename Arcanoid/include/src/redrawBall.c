ac5db3f1164953b0ac1f82a091d7421836fe311d
Morten Hilligsøe <mlhilligsoe@gmail.com>
2014-06-16 11:07:19 +0200

    Files managed

--- Arcanoid/include/src/redrawBall.c
+++
@@ -1,9 +0,0 @@
-#include <eZ8.h>
-#include <sio.h>
-
-void redrawBall(char x.old, char y.old, char x.new, char y.new){
-gotoxy(x.old, y.old);
-printf(" ");
-gotoxy(x.new, y.new);
-printf("O");
-}
--- dag3/include/graphics.h
+++ dag3/include/graphics.h
@@ -1,9 +1,9 @@
-#include <eZ8.h>             // special encore constants, macros and flash routines
-#include <sio.h>   
-#include "struct.h"
+#include <eZ8_h>             // special encore constants, macros and flash routines
+#include <sio_h>   
+#include "struct_h"
 
-void redrawPlayer(char x.old, char y.old, char x.new, char y.new);
-void redrawBall(char x.old, char y.old, char x.new, char y.new);
+void redrawPlayer(char x_old, char y_old, char x_new, char y_new);
+void redrawBall(char x_old, char y_old, char x_new, char y_new);
 void drawBlock(struct Block* block);
 void drawEdges();
 void drawLevel();
