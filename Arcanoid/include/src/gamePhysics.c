8815570689f7693f8d8988c31044a08c41e2452c
s130294 <lennartk@lk-macbook.local>
2014-06-16 14:15:25 +0200

    updateBallPosition() updated ;)

--- Arcanoid/include/src/gamePhysics.c
+++ Arcanoid/include/src/gamePhysics.c
@@ -6,7 +6,17 @@ updatePlayerPos(char input, struct Player* player){
 }
 
 updateBallPos(struct Ball* ball){
-
+    
+    char old_x = (char) (ball->x >> 16);
+    char old_y = (char) (ball->y >> 16);
+    ball->x = ball->x + ball->vx;
+    ball->y = ball->y + ball->vy;
+    char new_x = (char) (ball->x >>16);
+    char new_y = (char) (ball->y >>16);
+    
+    redrawBall(old_x, old_y, new_x, new_y);
+    
+    
 }
 
 checkWallCollision(struct Ball* ball, struct Level* level){
